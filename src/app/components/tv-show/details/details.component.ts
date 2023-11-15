e45693e7ce3dcb3b0a7ca4fdaa7ff72d682d5150
Leonardo Natal <leonardo.natal@dev.pro>
2023-11-15 13:12:27 -0300

    ex 04

--- src/app/components/tv-show/details/details.component.ts
+++ src/app/components/tv-show/details/details.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { TvShowDetails } from 'src/app/components/tv-show/models/tv-show-details.model';
+import { Episode, TvShowDetails } from 'src/app/components/tv-show/models/tv-show-details.model';
 
 @Component({
   selector: 'app-details',
@@ -12,6 +12,19 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.tvShowDetails);
+  }
+
+  getNumberOfSeasons(): number {
+    const episodes = this.tvShowDetails?.episodes || [];
+    const uniqueSeasons: number[] = [];
+
+    episodes.forEach((episode: Episode) => {
+      if (!uniqueSeasons.includes(episode.season)) {
+        uniqueSeasons.push(episode.season);
+      }
+    });
+
+    return uniqueSeasons.length;
   }
 
 }
